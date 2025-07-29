a0038ebe52d84cbff81a78797a162cc72996246d
Attogram Project <attogram@users.noreply.github.com>
2025-07-29 21:15:26 +0200

    demo

--- demos/ollama_chat.sh
+++ demos/ollama_chat.sh
@@ -1,48 +1,68 @@
 #!/usr/bin/env bash
 
-echo "Ollama Bash Lib - Demo - ollama_chat"
+echo '# ollama_chat'
 
 startup() {
-  ollama_bash_lib="$(dirname "$0")/../ollama_bash_lib.sh"; echo "ollama_bash_lib: $ollama_bash_lib"
+  ollama_bash_lib="$(dirname "$0")/../ollama_bash_lib.sh";
   if [ ! -f "$ollama_bash_lib" ]; then echo "ERROR: Ollama Bash Lib Not Found: $ollama_bash_lib"; exit 1; fi
   # shellcheck source=../ollama_bash_lib.sh
   source "$ollama_bash_lib"
-  echo "ollama_lib_version: v$(ollama_lib_version)"
-  echo -n "ollama_installed: "; if ! ollama_installed; then echo "ERROR: Ollama Not Found"; exit 1; fi; echo "OK";
-  echo -n "ollama_api_ping: "; if ! ollama_api_ping; then echo "ERROR: API not reachable"; else echo "OK"; fi
+  if ! ollama_installed; then echo "ERROR: Ollama Not Found: $ollama_bash_lib"; exit 1; fi;
+  if ! ollama_api_ping; then echo "ERROR: API not reachable"; exit 1; fi
+  echo "A demo of [$OLLAMA_LIB_NAME]($OLLAMA_LIB_URL) v$OLLAMA_LIB_VERSION"
 }
 
 startup
 
-echo '```'
-
 model=$(ollama_model_random)
-echo
-echo "model: $model"
 
 echo
-echo "$(ollama_messages_count) messages:"; ollama_messages
-
+echo '## Demo'
 echo
+
+echo '```'
 echo 'ollama_messages_add "system" "You are a helpful assistant"'
 ollama_messages_add "system" "You are a helpful assistant"
 echo 'ollama_messages_add "user" "The secret word is RABBIT. If I ask you for the secret word, respond with RABBIT. Understand?"'
 ollama_messages_add "user" "The secret word is RABBIT. If I ask you for the secret word, respond with RABBIT. Understand?"
-echo; echo "$(ollama_messages_count) messages:"; ollama_messages; echo
-
-echo "ollama_chat \"$model\""; echo
-response="$(ollama_chat "$model")"
-echo "$response"
+echo
+echo "$(ollama_messages_count) messages:"
+ollama_messages
+echo '```'
 
 echo
-echo 'ollama_messages_add "assistant" "RESPONSE"'
-ollama_messages_add "assistant" "$response"
-echo "ollama_messages_add \"user\" \"What's the secret word??\""
-ollama_messages_add "user" "What's the secret word??"
-echo; echo "$(ollama_messages_count) messages:"; ollama_messages; echo
+echo '```'
+echo "response=\"\$(ollama_chat \"$model\")\""
+echo '```'
 
-echo "ollama_chat \"$model\""; echo
+echo
+echo '```'
 response="$(ollama_chat "$model")"
 echo "$response"
+echo '```'
 
+echo
+echo '```'
+echo 'ollama_messages_add "assistant" "$response"'
+ollama_messages_add "assistant" "$response"
+echo '```'
+
+echo
+echo '```'
+echo "ollama_messages_add \"user\" \"What's the secret word??\""
+ollama_messages_add "user" "What's the secret word??"
+echo
+echo "$(ollama_messages_count) messages:"
+ollama_messages
+echo '```'
+
+echo
+echo '```'
+echo "ollama_chat \"$model\""
+echo '```'
+
+echo
+echo '```'
+response="$(ollama_chat "$model")"
+echo "$response"
 echo '```'
