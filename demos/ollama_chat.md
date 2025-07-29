db0d70b266f67cbeaad0824e5758102ab697726d
Attogram Project <attogram@users.noreply.github.com>
2025-07-29 21:38:40 +0200

    demo

--- demos/ollama_chat.md
+++ demos/ollama_chat.md
@@ -3,43 +3,47 @@ A demo of [Ollama Bash Lib](https://github.com/attogram/ollama-bash-lib) v0.38.1
 
 ## Demo
 
-```
+```bash
 ollama_messages_add "system" "You are a helpful assistant"
-ollama_messages_add "user" "The secret word is RABBIT. If I ask you for the secret word, respond with RABBIT. Understand?"
-
+ollama_messages_add "user" "The secret word is RABBIT. If asked for the secret word, respond with RABBIT. Understand?"
+echo "$(ollama_messages_count) messages:"
+ollama_messages
+echo
+ollama_chat "llava:7b"
+```
+```
 2 messages:
 {"role":"system","content":"You are a helpful assistant"}
 {"role":"user","content":"The secret word is RABBIT. If I ask you for the secret word, respond with RABBIT. Understand?"}
-```
 
+ Yes, I understand. Please let me know if there's anything else I can help you with! 
 ```
-response="$(ollama_chat "gemma3:1b")"
+```bash
+echo "$(ollama_messages_count) messages:"
+ollama_messages
 ```
-
 ```
-Understood. RABBIT. 
-
-Let’s play! 😊
-```
-
-```
-ollama_messages_add "assistant" "$response"
-```
-
-```
-ollama_messages_add "user" "What's the secret word??"
-
-4 messages:
+3 messages:
 {"role":"system","content":"You are a helpful assistant"}
 {"role":"user","content":"The secret word is RABBIT. If I ask you for the secret word, respond with RABBIT. Understand?"}
-{"role":"assistant","content":"Understood. RABBIT. \n\nLet’s play! 😊"}
-{"role":"user","content":"What's the secret word??"}
+{"role":"assistant","content":" Yes, I understand. Please let me know if there's anything else I can help you with! "}
 ```
-
+```bash
+ollama_messages_add "user" "What is the secret word??"
+ollama_chat "llava:7b"
 ```
-ollama_chat "gemma3:1b"
 ```
-
+ The secret word is "RABBIT". 
 ```
-RABBIT
+```bash
+echo "$(ollama_messages_count) messages:"
+ollama_messages
+```
+```
+5 messages:
+{"role":"system","content":"You are a helpful assistant"}
+{"role":"user","content":"The secret word is RABBIT. If I ask you for the secret word, respond with RABBIT. Understand?"}
+{"role":"assistant","content":" Yes, I understand. Please let me know if there's anything else I can help you with! "}
+{"role":"user","content":"What is the secret word??"}
+{"role":"assistant","content":" The secret word is \"RABBIT\". "}
 ```
--- demos/ollama_chat.sh
+++ demos/ollama_chat.sh
@@ -20,49 +20,46 @@ echo
 echo '## Demo'
 echo
 
-echo '```'
+echo '```bash'
 echo 'ollama_messages_add "system" "You are a helpful assistant"'
-ollama_messages_add "system" "You are a helpful assistant"
-echo 'ollama_messages_add "user" "The secret word is RABBIT. If I ask you for the secret word, respond with RABBIT. Understand?"'
-ollama_messages_add "user" "The secret word is RABBIT. If I ask you for the secret word, respond with RABBIT. Understand?"
-echo
-echo "$(ollama_messages_count) messages:"
-ollama_messages
-echo '```'
-
-echo
-echo '```'
-echo "response=\"\$(ollama_chat \"$model\")\""
-echo '```'
-
-echo
-echo '```'
-response="$(ollama_chat "$model")"
-echo "$response"
-echo '```'
-
-echo
-echo '```'
-echo 'ollama_messages_add "assistant" "$response"'
-ollama_messages_add "assistant" "$response"
-echo '```'
-
-echo
-echo '```'
-echo "ollama_messages_add \"user\" \"What's the secret word??\""
-ollama_messages_add "user" "What's the secret word??"
-echo
-echo "$(ollama_messages_count) messages:"
-ollama_messages
-echo '```'
-
-echo
-echo '```'
+echo 'ollama_messages_add "user" "The secret word is RABBIT. If asked for the secret word, respond with RABBIT. Understand?"'
+echo 'echo "$(ollama_messages_count) messages:"'
+echo 'ollama_messages'
+echo 'echo'
 echo "ollama_chat \"$model\""
 echo '```'
-
-echo
 echo '```'
-response="$(ollama_chat "$model")"
-echo "$response"
+ollama_messages_add "system" "You are a helpful assistant"
+ollama_messages_add "user" "The secret word is RABBIT. If I ask you for the secret word, respond with RABBIT. Understand?"
+echo "$(ollama_messages_count) messages:"
+ollama_messages
+echo
+ollama_chat "$model"
+echo '```'
+
+echo '```bash'
+echo 'echo "$(ollama_messages_count) messages:"'
+echo 'ollama_messages'
+echo '```'
+echo '```'
+echo "$(ollama_messages_count) messages:"
+ollama_messages
+echo '```'
+
+echo '```bash'
+echo 'ollama_messages_add "user" "What is the secret word??"'
+echo "ollama_chat \"$model\""
+echo '```'
+echo '```'
+ollama_messages_add "user" "What is the secret word??"
+ollama_chat "$model"
+echo '```'
+
+echo '```bash'
+echo 'echo "$(ollama_messages_count) messages:"'
+echo 'ollama_messages'
+echo '```'
+echo '```'
+echo "$(ollama_messages_count) messages:"
+ollama_messages
 echo '```'
