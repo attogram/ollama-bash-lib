9d4f9baae3277274198734f9d97f17b5fc0479ca
Attogram Project <attogram@users.noreply.github.com>
2025-07-31 00:30:42 +0200

    Welcome to Ollama Bash Lib

--- ollama_bash_lib.sh
+++ ollama_bash_lib.sh
@@ -4,7 +4,7 @@
 #
 
 OLLAMA_LIB_NAME="Ollama Bash Lib"
-OLLAMA_LIB_VERSION="0.40.4"
+OLLAMA_LIB_VERSION="0.40.5"
 OLLAMA_LIB_URL="https://github.com/attogram/ollama-bash-lib"
 OLLAMA_LIB_LICENSE="MIT"
 OLLAMA_LIB_COPYRIGHT="Copyright (c) 2025 Attogram Project <https://github.com/attogram>"
