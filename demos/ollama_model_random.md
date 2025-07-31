bbfc66efd45a948311936601296dc86d860ac2ac
Attogram Project <attogram@users.noreply.github.com>
2025-07-31 18:43:29 +0200

    ollama_model_random demo

---
+++ demos/ollama_model_random.md
@@ -0,0 +1,33 @@
+# ollama_model_random
+
+A [demo](../README.md#demos) of [Ollama Bash Lib](https://github.com/attogram/ollama-bash-lib) v0.40.5
+
+# Demo
+```bash
+ollama_model_random
+ollama_model_random
+ollama_model_random
+```
+```
+gemma:2b
+qwen2.5-coder:7b
+qwen2.5vl:3b
+```
+
+# Demo Debug
+```bash
+ollama_model_random
+ollama_model_random
+ollama_model_random
+```
+```
+[DEBUG] ollama_model_random
+[DEBUG] ollama_list_array
+qwen3:4b
+[DEBUG] ollama_model_random
+[DEBUG] ollama_list_array
+qwen3:1.7b
+[DEBUG] ollama_model_random
+[DEBUG] ollama_list_array
+llama3.1:8b-instruct-q4_1
+```
---
+++ demos/ollama_model_random.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo '# ollama_model_random'
+
+startup() {
+  ollama_bash_lib="$(dirname "$0")/../ollama_bash_lib.sh";
+  if [ ! -f "$ollama_bash_lib" ]; then echo "ERROR: Ollama Bash Lib Not Found: $ollama_bash_lib"; exit 1; fi
+  # shellcheck source=../ollama_bash_lib.sh
+  source "$ollama_bash_lib"
+  if ! ollama_installed; then echo "ERROR: Ollama Not Installed"; fi;
+  if ! ollama_api_ping; then echo "ERROR: Ollama API not reachable"; fi
+  echo; echo "A [demo](../README.md#demos) of [$OLLAMA_LIB_NAME]($OLLAMA_LIB_URL) v$OLLAMA_LIB_VERSION"
+}
+
+startup
+
+demo() {
+  echo '```bash'
+  echo 'ollama_model_random'
+  echo 'ollama_model_random'
+  echo 'ollama_model_random'
+  echo '```'
+  echo '```'
+  ollama_model_random
+  ollama_model_random
+  ollama_model_random
+  echo '```'
+}
+
+echo; echo '# Demo'
+demo
+
+echo; echo '# Demo Debug'
+OLLAMA_LIB_DEBUG=1 demo
