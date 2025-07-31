3de3257e3600f61e006353e94b71466c20fc62a8
Attogram Project <attogram@users.noreply.github.com>
2025-07-31 19:09:57 +0200

    OLLAMA_LIB_COPYRIGHT

--- ollama_bash_lib.sh
+++ ollama_bash_lib.sh
@@ -4,10 +4,10 @@
 #
 
 OLLAMA_LIB_NAME="Ollama Bash Lib"
-OLLAMA_LIB_VERSION="0.40.6"
+OLLAMA_LIB_VERSION="0.40.7"
 OLLAMA_LIB_URL="https://github.com/attogram/ollama-bash-lib"
 OLLAMA_LIB_LICENSE="MIT"
-OLLAMA_LIB_COPYRIGHT="Copyright (c) 2025 Attogram Project <https://github.com/attogram>"
+OLLAMA_LIB_COPYRIGHT="Copyright (c) 2025 Ollama Bash Lib, Attogram Project <https://github.com/attogram>"
 
 OLLAMA_LIB_API=${OLLAMA_HOST:-"http://localhost:11434"} # Ollama API URL, No slash at end
 OLLAMA_LIB_DEBUG=0     # 0 = No debug messages, 1 = Yes debug messages
