48e3f63191dbd5f132a8fdb111c1b6aaf70349b9
Attogram Project <attogram@users.noreply.github.com>
2025-08-04 20:18:27 +0200

    demo scripts

--- demos/version.sh
+++ demos/version.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-echo "# ollama_version, ollama_version_json, ollama_version_cli"
+echo "# ollama_app_version, ollama_app_version_json, ollama_app_version_cli"
 
 startup() {
   ollama_bash_lib="$(dirname "$0")/../ollama_bash_lib.sh";
   if [ ! -f "$ollama_bash_lib" ]; then echo "ERROR: Ollama Bash Lib Not Found: $ollama_bash_lib"; exit 1; fi
   # shellcheck source=../ollama_bash_lib.sh
   source "$ollama_bash_lib"
-  if ! ollama_installed; then echo "ERROR: Ollama Not Installed"; fi;
+  if ! ollama_app_installed; then echo "ERROR: Ollama Not Installed"; fi;
   if ! ollama_api_ping; then echo "ERROR: Ollama API not reachable"; fi
   echo; echo "A [demo](../README.md#demos) of [$OLLAMA_LIB_NAME]($OLLAMA_LIB_URL) v$OLLAMA_LIB_VERSION"
 }
@@ -17,18 +17,18 @@ startup
 echo '```'
 
 echo
-echo "ollama_version"
+echo "ollama_app_version"
 echo
-ollama_version
+ollama_app_version
 
 echo
-echo "ollama_version_json"
+echo "ollama_app_version_json"
 echo
-ollama_version_json | jq "."
+ollama_app_version_json | jq "."
 
 echo
-echo "ollama_version_cli"
+echo "ollama_app_version_cli"
 echo
-ollama_version_cli
+ollama_app_version_cli
 
 echo '```'
